2255aeabf7c263544a77a2a8fd737db76917a0dc
Михаил <bigrichman7@gmail.com>
2023-07-18 16:38:23 +0300

    base css in smarty static

--- movieman/views.py
+++ movieman/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Empty page')
+    return render(request, 'movieman/index.html')
